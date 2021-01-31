067156858f255122f22bb0e9ee5e0bbbd894b9d6
Euxcbsks <hawhaw02030746@gmail.com>
2021-01-31 23:06:05 +0800

    add submodule 'pywikibot'

--- tool_pack/__init__.py
+++ tool_pack/__init__.py
@@ -1,6 +1,11 @@
 import os
 
+from .exceptions import SubmoduleMissingError
+
 if 'mcp' not in os.listdir('.'):
-	pass
+	raise SubmoduleMissingError('Submodule "mcp" not found')
+
+if 'tam' not in listdir:
+	raise SubmoduleMissingError('Submodule "tam" not found')
 
 from .generate import *
---
+++ tool_pack/exceptions.py
@@ -0,0 +1,2 @@
+class SubmoduleMissingError(FileNotFoundError):
+	pass
