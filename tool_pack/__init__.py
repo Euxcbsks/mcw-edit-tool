7dfb0c4844ecb366c4845d34ae0ce8e11adadec8
Euxcbsks <hawhaw02030746@gmail.com>
2021-01-31 23:39:03 +0800

    submodule 'pywikibot' delete

--- tool_pack/__init__.py
+++ tool_pack/__init__.py
@@ -1,11 +1,18 @@
 import os
 
+from .mcp import mcp
+from .mcp.mcp.tools.path import run_here
+
 from .exceptions import SubmoduleMissingError
 
-if 'mcp' not in os.listdir('.'):
-	raise SubmoduleMissingError('Submodule "mcp" not found')
+def listdir():
+	return os.listdir('.')
 
-if 'tam' not in listdir:
-	raise SubmoduleMissingError('Submodule "tam" not found')
+with run_here(listdir) as dirlist:
+	if 'mcp' not in dirlist:
+		raise SubmoduleMissingError('Submodule "mcp" not found')
+	
+	if 'tam' not in dirlist:
+		raise SubmoduleMissingError('Submodule "tam" not found')
 
 from .generate import *
